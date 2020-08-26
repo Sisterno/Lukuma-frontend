cdcdc7ba8f6f047d335ba9ef627c4caaacec680e
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2020-08-26 08:34:47 -0500

    Maquetado de header :V

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,21 @@
+import styles from "../styles/header.module.css";
+import Link from "next/link";
 export default function Header(){
     return(
-        <header>
-            I'm the real Header
+        <header className = {styles.header_2}>
+            <div className="header-2" >
+                <nav>
+                    <img className={styles.logo} src="/lukuma_logo_ico.png"></img>
+                    
+                    <menu className={styles.menu_1}>
+                    <Link href="/"><a className = {styles.enlace}>Destinos</a></Link>
+                    <Link href="/"><a className = {styles.enlace}>Ideas</a></Link>
+                    <Link href="/"><a className = {styles.enlace}>Revistas</a></Link>
+                    <Link href="/"><a className = {styles.enlace}>Conceptos</a></Link>
+                    </menu>
+                </nav>
+            </div>
+            <div className={styles.covid_box}></div>
         </header>
     )
 }
