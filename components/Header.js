724f3020af625e45fcb01470d30241101eb9e3f8
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2020-08-26 12:19:08 -0500

    Correcciones en las etiquetas de los css y tambien cambios para que se vea mejor

--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,21 @@ import styles from "../styles/header.module.css";
 import Link from "next/link";
 export default function Header(){
     return(
-        <header className = {styles.header_2}>
-            <div className="header-2" >
-                <nav>
-                    <img className={styles.logo} src="/lukuma_logo_ico.png"></img>
-                    
-                    <menu className={styles.menu_1}>
-                    <Link href="/"><a className = {styles.enlace}>Destinos</a></Link>
-                    <Link href="/"><a className = {styles.enlace}>Ideas</a></Link>
-                    <Link href="/"><a className = {styles.enlace}>Revistas</a></Link>
-                    <Link href="/"><a className = {styles.enlace}>Conceptos</a></Link>
-                    </menu>
+        <header className = {styles.headerContain}>
+            <div className = {styles.menuContain}>
+                {/* <div className={styles.logoContent}> */}
+                    <img className={styles.logo} src="/lukuma_logo_ico.png"/>
+                {/* </div> */}
+                <nav className={styles.menuCustom1}> 
+                    {/* <menu> */}
+                    <Link href="/"><a className = {styles.linkCustom}>Destinos</a></Link>
+                    <Link href="/"><a className = {styles.linkCustom}>Ideas</a></Link>
+                    <Link href="/"><a className = {styles.linkCustom}>Revistas</a></Link>
+                    <Link href="/"><a className = {styles.linkCustom}>Conceptos</a></Link>
+                    {/* </menu> */}
                 </nav>
             </div>
-            <div className={styles.covid_box}></div>
+            <div className={styles.covidBox}></div>
         </header>
     )
 }
