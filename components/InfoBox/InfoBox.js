5ddda3cd9e18d3d1eb4b6c004ab2c8f7b6ba7bde
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2020-08-30 21:37:27 -0500

    InfoBox version ultimate - layout

--- components/InfoBox/InfoBox.js
+++ components/InfoBox/InfoBox.js
@@ -1,15 +1,42 @@
 import styles from "./InfoBox.module.css";
 import InfoBoxCard from './InfoBoxCard/InfoBoxCard'
+
+let dataPruebas = {
+    infoCard:[
+        {
+            infoImage:"",
+            infoTitle:"Viaje y colabore",
+            infoText:`Use the search tools here to find other families or people,
+            travelers like you, to meet up with on the road, and be
+            sure to register your trips so that they can find you, too!`,
+            infoButtonText:"Ingresar o crear cuenta"
+        },
+        {
+            infoImage:"",
+            infoTitle:"Administre sus viajes desde una cuenta",
+            infoText:`"When you're on the road for a long spell, whether as a
+            worldschooling family or a digital nomad, it can be hard to`,
+            infoButtonText:"Agregar un destino"
+        },
+        {
+            infoImage:"",
+            infoTitle:"Es proveedor de turismo unase ! a lukuma.pe",
+            infoText:`"When you're on the road for a long spell, whether as a
+            worldschooling family or a digital nomad, it can be hard to `,
+            infoButtonText:"Quiero ser parte de lukuma"
+        }
+    ]
+}
 export default function InfoBox(){
     return(
-        <div>
-            <div>
-                <img></img>
-                <span></span>
+        <div className={styles.InfoBoxContain}>
+            <div className={styles.LogoContain}>
+                <img className={styles.Logo} src="/lukuma_logo_ico.png"></img>
+                <span className={styles.LogoText}>Lukuma.pe</span>
             </div>
-            <InfoBoxCard/>
-            <InfoBoxCard/>
-            <InfoBoxCard/>
+            <InfoBoxCard info={dataPruebas.infoCard[0]}/>
+            <InfoBoxCard info={dataPruebas.infoCard[1]}/>
+            <InfoBoxCard info={dataPruebas.infoCard[2]}/>
         </div>
     )
 }
--- components/InfoBox/InfoBoxCard/InfoBoxCard.js
+++ components/InfoBox/InfoBoxCard/InfoBoxCard.js
@@ -1,7 +1,20 @@
 import styles from "./InfoBoxCard.module.css";
-export default function InfoBox(){
+export default function InfoBox({
+    info={
+        infoImage:"none",
+        infoTitle:"error",
+        infoText:"error",
+        infoButtonText:"error"
+    }
+}){
     return(
-        <div>
+        <div className={styles.InfoBoxCardBox}>
+            <img className={styles.InfoBoxCardImg} src={info.infoImage}/>
+            <span className={styles.InfoBoxCardTitle}>{info.infoTitle} </span>
+            <p className={styles.InfoBoxCardImgText}> {info.infoText} </p>
+            <div className={styles.InfoBoxCardImgButtom}>
+                <span className={styles.InfoBoxCardImgButtomText}> {info.infoButtonText} </span>
+            </div>            
         </div>
     )
 }
