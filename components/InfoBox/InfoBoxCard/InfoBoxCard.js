b93f42aafc61d43c0bd736ad7043e07b8a62fe9f
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2020-08-31 16:05:38 -0500

    New list tours - error in static data :vs

--- components/InfoBox/InfoBoxCard/InfoBoxCard.js
+++ components/InfoBox/InfoBoxCard/InfoBoxCard.js
@@ -1,15 +1,16 @@
 import styles from "./InfoBoxCard.module.css";
 export default function InfoBox({
     info={
-        infoImage:"none",
+        imgPath:"error",
         infoTitle:"error",
         infoText:"error",
         infoButtonText:"error"
     }
 }){
+    let img = info.infoImg
     return(
         <div className={styles.InfoBoxCardBox}>
-            <img className={styles.InfoBoxCardImg} src={info.infoImage}/>
+            <img className={styles.InfoBoxCardImg} src={`${info.infoTitle}`}/>
             <span className={styles.InfoBoxCardTitle}>{info.infoTitle} </span>
             <p className={styles.InfoBoxCardImgText}> {info.infoText} </p>
             <div className={styles.InfoBoxCardImgButtom}>
---
+++ components/LandingListTours/LandingListTours.js
@@ -0,0 +1,44 @@
+import styles from "./InfoBox.module.css";
+import InfoBoxCard from './InfoBoxCard/InfoBoxCard'
+
+let dataPruebas = {
+    infoCard:[
+        {
+            imgPath:"/image-not-found.png",
+            infoTitle:"Viaje y colabore 2",
+            infoText:`Use the search tools here to find other families or people,
+            travelers like you, to meet up with on the road, and be
+            sure to register your trips so that they can find you, too!`,
+            infoButtonText:"Ingresar o crear cuenta"
+        },
+        {
+            imgPath:"/image-not-found.png",
+            infoTitle:"Administre sus viajes desde una cuenta",
+            infoText:`"When you're on the road for a long spell, whether as a
+            worldschooling family or a digital nomad, it can be hard to`,
+            infoButtonText:"Agregar un destino"
+        },
+        {
+            imgPath:"/image-not-found.png",
+            infoTitle:"Es proveedor de turismo unase ! a lukuma.pe",
+            infoText:`"When you're on the road for a long spell, whether as a
+            worldschooling family or a digital nomad, it can be hard to `,
+            infoButtonText:"Quiero ser parte de lukuma"
+        }
+    ]
+}
+
+
+export default function InfoBox(){
+    return(
+        <div className={styles.InfoBoxContain}>
+            <div className={styles.LogoContain}>
+                <img className={styles.Logo} src="/lukuma_logo_ico.png"></img>
+                <span className={styles.LogoText}>Lukuma.pe</span>
+            </div>
+            <InfoBoxCard info={dataPruebas.infoCard[0]}/>
+            <InfoBoxCard info={dataPruebas.infoCard[1]}/>
+            <InfoBoxCard info={dataPruebas.infoCard[2]}/>
+        </div>
+    )
+}
