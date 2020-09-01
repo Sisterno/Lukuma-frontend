f1df63f72dbf2532759a91dfcbce4c7efcb2e83f
Your Name <you@example.com>
2020-09-01 16:23:51 -0500

    finish the section MostVisitedDestination Component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Hero from '../components/Hero/Hero'
 import Destination from '../components/FeaturedDestination/FeaturedDestination'
-import BestSites from '../components/BestSites'
 import InfoBox from '../components/InfoBox/InfoBox'
 import LandingListTours from '../components/LandingListTours/LandingListTours'
+import MostVisitedDestination from '../components/MostVisitedDestination/MostVisitedDestination'
 // import React,{useState,useEffect} from 'react'
 
 export default function Home() {
@@ -11,9 +11,9 @@ export default function Home() {
       <Hero/>
       <InfoBox/>
       <Destination/>
-      <BestSites/>
       <LandingListTours Title="Tours Recomendados"/>
       <LandingListTours Title="Experiencias inolvidables"/>
+      <MostVisitedDestination/>
     </div>
   )
 }
